6465691cfa54d30d4f23c1afd8fcdb5db836df1d
wesrumfelt <wes.rumfelt@gmail.com>
2019-11-15 12:17:57 -0500

    10th commit

--- app/services/Stock.scala
+++ app/services/Stock.scala
@@ -4,21 +4,24 @@ import java.io.FileNotFoundException
 
 import yahoofinance.{Stock, YahooFinance}
 import java.math.BigDecimal
+import org.slf4j.{Logger, LoggerFactory}
 
 class Stock(val stockId: String) {
   //TODO : utilize dependency injection to inject the yahoo finance
+    def log : Logger = LoggerFactory.getLogger(classOf[Stock])
     def isValidStock() : Boolean = {
       try {
         val stock: yahoofinance.Stock = YahooFinance.get(stockId)
       } catch {
         case x: FileNotFoundException => {
-          println("Exception: Stock not found")
+          log.warn(s"Exception: Stock ($stockId) not found")
         }
         return false
       }
       return true
     }
     def getStockVal() : BigDecimal = {
+      log.info(s"Getting stock val for $stockId")
       val stock: yahoofinance.Stock = YahooFinance.get(stockId)
       stock.getQuote.getPrice
     }
